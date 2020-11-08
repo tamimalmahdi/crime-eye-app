c120a2b4ce64788c706c53106f222491b7f7d95d
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-11-08 16:53:05 +0000

    Added: Home, About & Products with seeds nav bar & footer tests successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'about/about'
+  get 'home/home'
+  root 'home#home'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
