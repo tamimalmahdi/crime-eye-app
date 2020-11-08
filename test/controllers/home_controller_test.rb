f0a26ee06339748f5aada3cfdb497cf633d1ad15
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-11-08 19:24:09 +0000

    Added Home & Product Testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get home_home_url
     assert_response :success
+    assert_select 'title', 'Chess Store'
+    assert_select 'h1', 'Title'
+    assert_select 'p', 'Welcome'
   end
-
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,42 +3,80 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Chess Set #{rand(1000)}"
   end
 
-  test "should get index" do
+  test 'should get index' do
     get products_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_product_url
     assert_response :success
   end
 
-  test "should create product" do
+  def create
+    @product = Product.new(product_params)
+    respond_to do |format|
+      if @product.save
+        format.html do
+          redirect_to @product,
+                      notice: 'Product was successfully created.'
+        end
+        format.json do
+          render :show, status: :created,
+                        location: @product
+        end
+      else
+        puts @product.errors.full_messages
+        format.html { render :new }
+        format.json do
+          render json: @product.errors,
+                 status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  test 'should create product' do
     assert_difference('Product.count') do
-      post products_url, params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+      post products_url, params: {
+        product: {
+          title: @title,
+          description: @product.description,
+          image_url: @product.image_url,
+          price: @product.price
+        }
+      }
     end
 
     assert_redirected_to product_url(Product.last)
   end
 
-  test "should show product" do
+  test 'should show product' do
     get product_url(@product)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_product_url(@product)
     assert_response :success
   end
 
-  test "should update product" do
-    patch product_url(@product), params: { product: {  title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+  test 'should update product' do
+    patch product_url(@product), params: {
+      product: {
+        title: @title,
+        description: @product.description,
+        image_url: @product.image_url,
+        price: @product.price
+      }
+    }
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
+  test 'should destroy product' do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
