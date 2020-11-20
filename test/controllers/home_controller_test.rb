be66423438aefa5e538526ce5445a71347a3cc0e
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-11-20 16:21:52 +0000

    Initial remote commit

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,4 +11,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select '.home ul.catalog li', 3
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
+
+  test 'should get nav' do
+    assert_select 'title', 'Chess Central'
+    assert_select '#myNavbar topnav a', minimum: 4
+    assert_select '#mySidebar sidebar a', minimum: 4
+  end
 end
