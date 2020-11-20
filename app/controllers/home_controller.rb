062789acb65f482b7dd63ea540299b88d76fe24c
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-11-20 15:19:48 +0000

    Added Catalog to home page with working tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def home
+    @products = Product.order(:title)
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,8 +4,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get home_home_url
     assert_response :success
-    assert_select 'title', 'Chess Store'
-    assert_select 'h1', 'Chess Store'
+    assert_select 'title', 'Chess Central'
+    assert_select 'h1', 'Chess Central'
     assert_select 'p', 'Welcome to my chess store!'
+    assert_select 'h1', 'Chess Central'
+    assert_select '.home ul.catalog li', 3
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 end
