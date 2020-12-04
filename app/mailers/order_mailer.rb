e9b527912aa632098d8884ab58eb991a571df4e2
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-12-04 19:05:04 +0000

    Added Mailer for orders, not working completely

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,24 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Chess Central <chess@example.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Chess Central Order Received'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Chess Central Order Shipped'
+  end
+end
