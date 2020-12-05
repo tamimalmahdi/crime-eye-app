0821a309647f423ff37189e37f59fd771f8f3868
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-12-05 00:03:06 +0000

    Added sessions with working routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
   resources :carts
+
   get 'about/about'
   get 'home/home'
   root 'home#home'
-  resources :products
+
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
