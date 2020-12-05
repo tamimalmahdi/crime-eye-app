49fde18ad76a3f0f96a1a5850ce92fda9d5304b3
Tamim Almahdi <ta00687@surrey.ac.uk>
2020-12-05 00:11:30 +0000

    Worked on controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+  before_action :authorize
+
+  protected
+
+  def authorize
+    redirect_to login_url, notice: 'Please log in' unless User.find_by(id: session[:user_id])
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize
   before_action :set_cart
   def home
     @products = Product.order(:title)
